55453edd4e2122373ea3f62388c6c1c1c7c753b8
Neal Archival <73256760+nealarch01@users.noreply.github.com>
2023-07-24 18:48:48 -0700

    Initial Commit

---
+++ TipApp/TipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipAppApp.swift
+//  TipApp
+//
+//  Created by Neal Archival on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
